bad2718e279ea002a55b50ce48a3aa456c38f8a9
faisallyt <fdiledu@gmail.com>
2024-10-31 20:09:46 +0530

    project initialized

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { CgProfile } from "react-icons/cg";
+import { FaCartShopping } from "react-icons/fa6";
+
+const Navbar = () => {
+  return (
+    <div className="flex px-4 py-4 justify-between items-center shadow-xl">
+      <div>
+        <h1 className=" text-xl">Grocery Store </h1>
+      </div>
+      <div className=" flex gap-6">
+        <FaCartShopping size={30} />
+        <CgProfile size={30} />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
